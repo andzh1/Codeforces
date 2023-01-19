e9f0cd4095a7d06259a1cd36e189f97a60f3bd0f
andzh1 <andrey.zhizhelev@yandex.ru>
2023-01-19 17:27:10 +0300

    Add round #832

---
+++ Round #832/C.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int int64_t
+#define get(a) int a; cin >> a;
+#define loop(n) for(int i = 0; i < n; ++i)
+#define i_am_speed std::ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+int modpow(const int& x, const int& power, const int& mod) {
+    if (power == 0) return 1;
+    if (power == 1) return x;
+    int sqrt = modpow(x, power / 2, mod);
+    sqrt *= sqrt;
+    sqrt %= mod;
+    if (power % 2) sqrt *= x;
+    sqrt %= mod;
+    return sqrt;
+}
+
+template<typename T = int>
+vector<T> read(int n) {
+    vector<T> v (n);
+    for (T& x : v) cin >> x;
+    return v;
+}
+
+template<typename T = int>
+vector<vector<T>> read(int n, int m) {
+    vector<vector<T>> v (n, vector<T> (m));
+    for (auto& x : v) x = read<T>(n);
+    return v;
+}
+
+template<typename T = int>
+void print(const vector<T>& v) {
+    for (const auto& x : v) cout << x << ' ';
+    cout << '\n';
+}
+
+template<typename T = int>
+void print(const vector<vector<T>>& v) {
+    for (const auto& x : v) print(x);
+}
+
+
+void solve_test_case() {
+    get(n)
+    auto v = read(n);
+    int min_ind = 0;
+    loop(n) {
+        if (v[i] < v[min_ind]) min_ind = i;
+    }
+    if (min_ind == 0) {
+        cout << "Bob\n";
+        return;
+    }
+    cout << "Alice\n";
+}
+
+
+signed main() {
+    i_am_speed
+    // freopen("tests.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int tests = 1;
+    cin >> tests;
+    while(tests--) solve_test_case();
+}
