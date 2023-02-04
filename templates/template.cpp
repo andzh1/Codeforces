bd07dac4e9635cf37b71ee1fbc517b6d4fa09d23
andzh1 <andrey.zhizhelev@yandex.ru>
2023-02-04 12:57:13 +0300

    Add templates

---
+++ templates/template.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int int64_t
+#define get(a) int a; cin >> a;
+#define loop(j, n) for(int j = 0; j < n; ++j)
+#define iloop(n) loop(i, n)
+#define jloop(n) loop(j, n)
+#define kloop(n) loop(j, n)
+#define fast_and_furious std::ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+int modpow(const int& x, const int& power, const int& mod) {
+    if (power == 0) return 1;
+    if (power == 1) return x;
+    int sqrt = modpow(x, power / 2, mod);
+    sqrt *= sqrt;
+    sqrt %= mod;
+    if (power % 2) sqrt *= x;
+    sqrt %= mod;
+    return sqrt;
+}
+
+struct SparceTable {
+    vector<vector<int>> data_;
+
+    SparceTable(const vector<int>& v) {
+        int n = v.size();
+        int logn = log2(n) + 1;
+        data_ = vector<vector<int>>(logn + 1, vector<int>(n));
+        data_[0] = v;
+        for (int i = 1; i <= logn; ++i) {
+            int pow = (1 << (i - 1));
+            for (int j = 0; j + pow < n; ++j) {
+                data_[i][j] = std::min(data_[i - 1][j], data_[i - 1][j + pow]);
+            }
+        }
+    }
+
+    // [left, right]
+    int GetMinOnSegment(int left, int right) const {
+        int len = right - left + 1;
+        if (len == 1) return data_[0][left];
+        int pow = 0;
+        while ((1 << (pow + 1)) < len) ++pow;
+        int ans1 = data_[pow][left];
+        int ans2 = data_[pow][right - (1 << pow) + 1];
+        return std::min(ans1, ans2);
+    }
+
+    // True, if on (left, right) interval there are no value greater then on left and right position
+    bool NoMaxOnInterval(int left, int right) const {
+        if (right - left == 1) {
+            return true;
+        }
+        return GetMinOnSegment(left + 1, right - 1) <= data_[0][left]; // Assume that v[left] == v[right]
+    }
+};
+
+
+template<typename T = int>
+vector<T> read(int n) {
+    vector<T> v (n);
+    for (T& x : v) cin >> x;
+    return v;
+}
+
+template<typename T = int>
+vector<vector<T>> read(int n, int m) {
+    vector<vector<T>> v (n, vector<T> (m));
+    for (auto& x : v) x = read<T>(n);
+    return v;
+}
+
+template<typename T = int>
+void print(const vector<T>& v) {
+    for (const auto& x : v) cout << x << ' ';
+    cout << '\n';
+}
+
+template<typename T = int>
+void print(const vector<vector<T>>& v) {
+    for (const auto& x : v) print(x);
+}
+
+
+void solve_test_case() {
+    get(n)
+    auto v = read(n);
+    sort(v.begin(), v.end());
+    if (v[0] == v.back()) {
+        cout << "NO\n";
+        return;
+    }
+    cout << "YES\n";
+    for (int i = 0; i < n/2; ++i) {
+        cout << v[n - 1 - i] << ' ' << v[i] << ' ';
+    }
+    if (n % 2) {
+        cout << v[n/2];
+    }
+    cout << '\n';
+}
+
+
+signed main() {
+    fast_and_furious
+    // freopen("tests.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int tests = 1;
+    cin >> tests;
+    while(tests --> 0) solve_test_case();
+}
