ab12d9f5a7270bba270125a923e0d8f2f08b83fe
andzh1 <andrey.zhizhelev@yandex.ru>
2023-05-26 10:24:49 +0300

    Educational round #149

---
+++ Educational round #149/D.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define int int64_t
+#define get(a) \
+  int a;       \
+  cin >> a;
+#define loop(j, n) for (int j = 0; j < n; ++j)
+#define iloop(n) loop(i, n)
+#define jloop(n) loop(j, n)
+#define kloop(n) loop(j, n)
+#define all(v) (v.begin(), v.end())
+#define rall(v) (v.end(), v.begin())
+#define foreach(v) for (const auto& x : v)
+#define fast_and_furious                                   \
+  std::ios::sync_with_stdio(false), std::cin.tie(nullptr), \
+      std::cout.tie(nullptr);
+
+int modpow(const int& x, const int& power, const int& mod) {
+  if (power == 0) return 1;
+  if (power == 1) return x;
+  int sqrt = modpow(x, power / 2, mod);
+  sqrt *= sqrt;
+  sqrt %= mod;
+  if (power % 2) sqrt *= x;
+  sqrt %= mod;
+  return sqrt;
+}
+
+template <typename T = int>
+vector<T> read(int n) {
+  vector<T> v(n);
+  for (T& x : v) cin >> x;
+  return v;
+}
+
+template <typename T = int>
+vector<vector<T>> read(int n, int m) {
+  vector<vector<T>> v(n, vector<T>(m));
+  for (auto& x : v) x = read<T>(n);
+  return v;
+}
+
+template <typename T>
+void print(const T& v) {
+  cout << v << ' ';
+}
+
+template <typename T = int>
+void print(const vector<T>& v) {
+  for (const auto& x : v) print(x);
+  cout << '\n';
+}
+
+template <typename T = int>
+void print(const vector<vector<T>>& v) {
+  for (const auto& x : v) print(x);
+}
+
+template <typename T>
+void sort_v(vector<T>& v) {
+  sort(all(v));
+}
+
+template <typename T>
+void rsort_v(vector<T>& v) {
+  sort(rall(v));
+}
+
+template <typename AxisType = int>
+struct Point {
+  AxisType x = 0;
+  AxisType y = 0;
+  int index = 0;
+  bool first_compare_by_x_ = true;
+
+  explicit Point(AxisType x, AxisType y) : x(x), y(y) {}
+  explicit Point(AxisType x, AxisType y, bool cmp)
+      : x(x), y(y), first_compare_by_x_(cmp) {}
+  explicit Point(AxisType x, AxisType y, int index)
+      : x(x), y(y), index(index) {}
+  explicit Point(AxisType x, AxisType y, int index, bool cmp)
+      : x(x), y(y), index(index), first_compare_by_x_(cmp) {}
+
+  bool operator<(const Point& other) const {
+    if (first_compare_by_x_) {
+      return (x < other.x) || (x == other.x && y < other.y) ||
+             (x == other.x && y == other.y && index < other.index);
+    } else {
+      return (y < other.y) || (y == other.y && x < other.x) ||
+             (x == other.x && y == other.y && index < other.index);
+    }
+  }
+};
+
+int get_balance(const string& s) {
+  int balance = 0;
+  foreach (s) {
+    if (x == '(') ++balance;
+    if (x == ')') --balance;
+  }
+  return balance;
+}
+
+bool is_cps(const string& s) {
+  int balance = 0;
+  foreach (s) {
+    if (x == '(') ++balance;
+    if (x == ')') --balance;
+    if (balance < 0) return false;
+  }
+  return balance == 0;
+}
+
+bool is_beautiful(const string& s) {
+  string cp = s;
+  if (is_cps(cp)) return true;
+  std::reverse(cp.begin(), cp.end());
+  if (is_cps(cp)) return true;
+  return false;
+}
+
+void solve_test_case() {
+  get(n) string s;
+  cin >> s;
+  if (n % 2 == 1 || get_balance(s) != 0) {
+    cout << "-1\n";
+    return;
+  }
+  if (is_beautiful(s)) {
+    cout << "1\n";
+    print(vector<int>(n, 1));
+    return;
+  }
+  cout << "2\n";
+  vector<int> coloring(n, 1);
+  int ip = n - 1;
+  for (int i = 0; i < ip; ++i) {
+    if (s[i] == ')') continue;
+    while (i < ip && s[ip] == '(') --ip;
+    if (i == ip) break;
+    coloring[i] = 2;
+    coloring[ip] = 2;
+    --ip;
+  }
+  print(coloring);
+}
+
+signed main() {
+  fast_and_furious
+      // freopen("tests.txt", "r", stdin);
+      // freopen("output.txt", "w", stdout);
+      int tests = 1;
+  cin >> tests;
+  while (tests-- > 0) solve_test_case();
+}
---
+++ Educational round #149/E.cpp
@@ -0,0 +1,186 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define int int64_t
+#define get(a) \
+  int a;       \
+  cin >> a;
+#define loop(j, n) for (int j = 0; j < n; ++j)
+#define iloop(n) loop(i, n)
+#define jloop(n) loop(j, n)
+#define kloop(n) loop(j, n)
+#define all(v) (v.begin(), v.end())
+#define rall(v) (v.end(), v.begin())
+#define foreach(v) for (const auto& x : v)
+#define fast_and_furious                                   \
+  std::ios::sync_with_stdio(false), std::cin.tie(nullptr), \
+      std::cout.tie(nullptr);
+
+int modpow(const int& x, const int& power, const int& mod) {
+  if (power == 0) return 1;
+  if (power == 1) return x;
+  int sqrt = modpow(x, power / 2, mod);
+  sqrt *= sqrt;
+  sqrt %= mod;
+  if (power % 2) sqrt *= x;
+  sqrt %= mod;
+  return sqrt;
+}
+
+template <typename T = int>
+vector<T> read(int n) {
+  vector<T> v(n);
+  for (T& x : v) cin >> x;
+  return v;
+}
+
+template <typename T = int>
+vector<vector<T>> read(int n, int m) {
+  vector<vector<T>> v(n, vector<T>(m));
+  for (auto& x : v) x = read<T>(n);
+  return v;
+}
+
+template <typename T>
+void print(const T& v) {
+  cout << v << ' ';
+}
+
+template <typename T = int>
+void print(const vector<T>& v) {
+  for (const auto& x : v) print(x);
+  cout << '\n';
+}
+
+template <typename T = int>
+void print(const vector<vector<T>>& v) {
+  for (const auto& x : v) print(x);
+}
+
+template <typename T>
+void sort_v(vector<T>& v) {
+  sort(all(v));
+}
+
+template <typename T>
+void rsort_v(vector<T>& v) {
+  sort(rall(v));
+}
+
+template <typename AxisType = int>
+struct Point {
+  AxisType x = 0;
+  AxisType y = 0;
+  int index = 0;
+  bool first_compare_by_x_ = true;
+
+  explicit Point(AxisType x, AxisType y) : x(x), y(y) {}
+  explicit Point(AxisType x, AxisType y, bool cmp)
+      : x(x), y(y), first_compare_by_x_(cmp) {}
+  explicit Point(AxisType x, AxisType y, int index)
+      : x(x), y(y), index(index) {}
+  explicit Point(AxisType x, AxisType y, int index, bool cmp)
+      : x(x), y(y), index(index), first_compare_by_x_(cmp) {}
+
+  bool operator<(const Point& other) const {
+    if (first_compare_by_x_) {
+      return (x < other.x) || (x == other.x && y < other.y) ||
+             (x == other.x && y == other.y && index < other.index);
+    } else {
+      return (y < other.y) || (y == other.y && x < other.x) ||
+             (x == other.x && y == other.y && index < other.index);
+    }
+  }
+};
+
+
+int place_by_number(int num) {
+    int place = 0;
+    while ((1 << place) < num) {
+        ++place;
+    }
+    return place - 1;
+}
+
+int MODULO = 998'244'353;
+
+vector<int> precalc_factorial((1 << 19) + 5, 1);
+
+int factorial(int n) {
+    return precalc_factorial[n];
+}
+
+bool between(int x, int l, int r) {
+    return x >= l && x <= r;
+}
+
+void solve_test_case() {
+    get(k)
+    int n = (1 << k);
+    auto seed = read(n);
+
+    vector<vector<int>> tree(k + 1);
+
+    for (int i = 0; i <= k; ++i) {
+        tree[i].resize(1 << i, -1);
+    }
+
+    for(int i = 0; i < n; ++i) {
+        if (seed[i] == -1) continue;
+        int num = seed[i];
+        int pos = i;
+        int xx = place_by_number(num);
+        for (int level = k; level > xx; --level) {
+            if (tree[level][pos] != -1) {
+                cout << "0\n";
+                return;
+            }
+            tree[level][pos] = num;
+            pos /= 2;
+        }
+    }
+    int answer = 1;
+    tree[0][0] = 1;
+    for (int level = 1; level <= k; ++level) {
+        int start = (1 << (level - 1)) + 1, end = start + (1 << (level - 1)) - 1; // [start, end] - upcoming new participants
+        int cur = 1, fact = (1 << (level - 1));
+        for (int i = 0; i < tree[level].size() - 1; i += 2) {
+            if (between(tree[level][i], start, end) && between(tree[level][i + 1], start, end)) {
+                cout << "0\n";
+                return;
+            }
+            int cnt = (tree[level][i] == -1) + (tree[level][i + 1] == -1);
+            cur *= ((cnt == 2) + 1);
+            cur %= MODULO;
+        }
+        answer *= cur;
+        answer %= MODULO;
+        for (int i = 0; i < tree[level].size(); ++i) {
+            if (between(tree[level][i], start, end)) {
+                --fact;
+            }
+        }
+        answer *= factorial(fact);
+        answer %= MODULO;
+    }
+    cout << answer << '\n';
+    
+}
+
+signed main() {
+  fast_and_furious
+      // freopen("tests.txt", "r", stdin);
+      // freopen("output.txt", "w", stdout);
+      int tests = 1;
+//   cin >> tests;
+  for (int i = 1; i < precalc_factorial.size(); ++i) {
+    precalc_factorial[i] = precalc_factorial[i - 1] * i;
+    precalc_factorial[i] %= MODULO;
+  }
+  while (tests-- > 0) solve_test_case();
+}
